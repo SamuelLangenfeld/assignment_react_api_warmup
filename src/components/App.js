054b9c8cb411f8a09c1d2de8857d7fe1cc692984
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-12-20 11:58:34 -0500

    initial

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import JumbotronFluid from './elements/JumbotronFluid'
+import UserList from './UserList'
+import UserForm from './UserForm'
+
+const App = ({users, isFetching, error, onAddUser, onDeleteUser}) => (
+  <div className="App">
+    <JumbotronFluid
+      heading="User CRUD"
+      lead="Using an API for User CRUD operations in React Applications"
+    />
+    <UserList
+      users={users}
+      onDelete={onDeleteUser}
+      isFetching={isFetching}
+    />
+    <br />
+    <UserForm
+      onSubmit={onAddUser}
+      error={error}
+    />
+  </div>
+)
+
+export default App
---
+++ src/components/UserCard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+// Custom card component for each user's data
+const UserCard = ({user, onDelete}) => {
+  const {first_name, last_name, avatar} = user
+
+  // Set the CSS max-width attribute directly in the
+  // element. `style` accepts a JS object and the
+  // attributes use camelcase. See docs for more info.
+  // Also using new card class for Bootstrap 4.
+  return (
+    <div
+      className="UserCard card"
+      style={{maxWidth: '128px'}}
+    >
+      <img
+        className="card-img-top img-fluid"
+        src={avatar}
+        alt="user avatar"
+      />
+      <div className="card-block">
+        <h4>{first_name} {last_name}</h4>
+      </div>
+       <div className="card-block" onClick={onDelete} id={this.props.key}>
+        <a><h4>Delete User</h4></a>
+      </div>
+    </div>
+  )
+}
+
+export default UserCard
